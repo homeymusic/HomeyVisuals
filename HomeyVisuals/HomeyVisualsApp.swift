1c95b66e65cea26877dfd919c3c088bad6b18fbc
Brian Mulloy <brian@landlessness.net>
2024-03-22 10:07:06 -0400

    Initial Commit

---
+++ HomeyVisuals/HomeyVisualsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeyVisualsApp.swift
+//  HomeyVisuals
+//
+//  Created by Brian McAuliff Mulloy on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeyVisualsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
