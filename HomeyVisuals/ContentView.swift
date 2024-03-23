2ce81921fe5726a8b1a50f495dff021edd7be0ef
Brian Mulloy <brian@landlessness.net>
2024-03-22 22:21:26 -0400

    changed icons to be more symmetrical with core octave

--- HomeyVisuals/ContentView.swift
+++ HomeyVisuals/ContentView.swift
@@ -36,48 +36,205 @@ struct ContentView: View {
 
             Spacer()
 
-            HStack(alignment: .bottom, spacing: 50) {
+            HStack(alignment: .bottom, spacing: 9) {
+                Image("home_far")           // P8
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(48) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(48))
+                    .scaleEffect(x: -1)
+                Image("stone_blue_far")     // m6
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(49) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(49))
+                    .scaleEffect(x: -1)
+                Image("stone_gold_far")     // M6
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(50) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(50))
+                    .scaleEffect(x: -1)
+                Image("diamond_blue_far")   // m7
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(51) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(51))
+                    .scaleEffect(x: -1)
+                Image("diamond_gold_far")   // M7
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(52) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(52))
+                    .scaleEffect(x: -1)
+                Image("tent_far")           // P5
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(53) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(53))
+                    .scaleEffect(x: -1)
+                Image("stone_orange_far")       // tt
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(54) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(54))
+                    .scaleEffect(x: -1)
+                Image("tent_far")           // P5
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(55) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(55))
+                    .scaleEffect(x: -1)
+                Image("diamond_blue_far")   // m7
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(56) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(56))
+                    .scaleEffect(x: -1)
+                Image("diamond_gold_far")   // M7
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(57) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(57))
+                    .scaleEffect(x: -1)
+                Image("stone_blue_far")     // m6
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(58) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(58))
+                    .scaleEffect(x: -1)
+                Image("stone_gold_far")     // M6
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(59) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(59))
+                    .scaleEffect(x: -1)
                 Image("home")               // P1
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(60) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(60))
                 Image("stone_blue")         // m2
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(61) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(61))
                 Image("stone_gold")         // M2
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(62) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(62))
                 Image("diamond_blue")       // m3
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(63) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(63))
                 Image("diamond_gold")       // M3
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(64) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(64))
                 Image("tent")               // P4
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(65) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(65))
                 Image("stone_orange")       // tt
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(66) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(66))
-                Image("tent_far")           // P5
+                Image("tent")               // P4
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(67) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(67))
-                Image("diamond_blue_far")   // m7
+                Image("diamond_blue")       // m3
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(68) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(68))
-                Image("diamond_gold_far")   // M7
+                Image("diamond_gold")       // M3
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(69) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(69))
-                Image("stone_blue_far")     // m6
+                Image("stone_blue")         // m2
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(70) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(70))
-                Image("stone_gold_far")     // M6
+                Image("stone_gold")         // M2
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(71) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(71))
-                Image("home_far")           // P8
+                Image("home")           // P8
+                    .resizable()
+                    .scaledToFit()
                     .offset(y: midiHelper.turnedOnNotes.contains(72) ? -300 : 0 )
                     .animation(.spring(), value: midiHelper.turnedOnNotes.contains(72))
+                Image("stone_blue_far")     // m6
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(73) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(73))
+                Image("stone_gold_far")     // M6
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(74) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(74))
+                Image("diamond_blue_far")   // m7
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(75) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(75))
+                Image("diamond_gold_far")   // M7
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(76) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(76))
+                Image("tent_far")           // P5
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(77) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(77))
+                Image("stone_orange_far")       // tt
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(78) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(78))
+                Image("tent_far")           // P5
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(79) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(79))
+                Image("diamond_blue_far")   // m7
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(80) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(80))
+                Image("diamond_gold_far")   // M7
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(81) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(81))
+                Image("stone_blue_far")     // m6
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(82) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(82))
+                Image("stone_gold_far")     // M6
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(83) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(83))
+                Image("home_far")           // P8
+                    .resizable()
+                    .scaledToFit()
+                    .offset(y: midiHelper.turnedOnNotes.contains(84) ? -300 : 0 )
+                    .animation(.spring(), value: midiHelper.turnedOnNotes.contains(84))
             }
-            .scaleEffect(x: xScaleEffect)
 
             Spacer()
 
