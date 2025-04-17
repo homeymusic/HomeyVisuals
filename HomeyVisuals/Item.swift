c788b45b2d3e6ee411bbf93e86e5c2f985281a24
Brian Mulloy <brian@landlessness.net>
2025-04-17 10:18:27 -0400

    xcode created the shell of an app

---
+++ HomeyVisuals/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HomeyVisuals
+//
+//  Created by Brian McAuliff Mulloy on 4/17/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
