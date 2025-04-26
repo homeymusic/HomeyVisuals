61b64ccd4e3521986efd922e62e9b6886198462e
Brian Mulloy <brian@landlessness.net>
2025-04-26 10:40:43 -0400

    no more queesires it breaks presentation

--- HomeyVisuals/Views/Widget/WidgetList.swift
+++ HomeyVisuals/Views/Widget/WidgetList.swift
@@ -11,41 +11,10 @@ struct WidgetList: View {
     let scale: CGFloat
     let widgetViewStyle: WidgetViewStyle
 
-    @Query private var textWidgets: [TextWidget]
-    @Query private var instrumentWidgets: [InstrumentWidget]
-
-    private var allWidgets: [any Widget] {
-      (textWidgets as [any Widget] + instrumentWidgets as [any Widget])
-        .sorted { $0.z < $1.z }
-    }
-
-    init(slide: Slide, scale: CGFloat, widgetViewStyle: WidgetViewStyle) {
-      self.slide = slide
-      self.scale = scale
-      self.widgetViewStyle = widgetViewStyle
-
-     let slideID: PersistentIdentifier = slide.persistentModelID
-        
-      // Compare relationship directly:
-      _textWidgets = Query(
-        filter:    #Predicate<TextWidget> { textWidget in
-            textWidget.slide?.persistentModelID == slideID
-        },
-        sort:      [SortDescriptor(\.z)],
-        animation: .default
-      )
-      _instrumentWidgets = Query(
-        filter:    #Predicate<InstrumentWidget> { instrumentWidget in
-            instrumentWidget.slide?.persistentModelID == slideID
-        },
-        sort:      [SortDescriptor(\.z)],
-        animation: .default
-      )
-    }
 
     var body: some View {
         ZStack(alignment: .topLeading) {
-            ForEach(allWidgets, id: \.id) { widget in
+            ForEach(slide.widgets, id: \.id) { widget in
                 switch widgetViewStyle {
                 case .thumbnail:
                     WidgetThumbnail(widget: widget, scale: scale)
