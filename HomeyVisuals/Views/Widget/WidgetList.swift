95a9d003fb176473fbc5c3fe6e4c73e3e7fa6526
Brian Mulloy <brian@landlessness.net>
2025-04-26 10:36:43 -0400

    query in WidgetList

--- HomeyVisuals/Views/Widget/WidgetList.swift
+++ HomeyVisuals/Views/Widget/WidgetList.swift
@@ -1,21 +1,51 @@
 // SlideWidgetsOverlay.swift
 
 import SwiftUI
+import SwiftData
 import HomeyMusicKit
 
 /// Renders *all* of a slide’s widgets in z-order, using the right widget view,
-/// and optionally allows interaction (hit-testing).
+/// and automatically stays up to date as SwiftData changes your model.
 struct WidgetList: View {
-    let slide: Slide
+    @Bindable var slide: Slide
     let scale: CGFloat
     let widgetViewStyle: WidgetViewStyle
-    
-    @Environment(AppContext.self) private var appContext
-    @Environment(InstrumentalContext.self) private var instrumentalContext
+
+    @Query private var textWidgets: [TextWidget]
+    @Query private var instrumentWidgets: [InstrumentWidget]
+
+    private var allWidgets: [any Widget] {
+      (textWidgets as [any Widget] + instrumentWidgets as [any Widget])
+        .sorted { $0.z < $1.z }
+    }
+
+    init(slide: Slide, scale: CGFloat, widgetViewStyle: WidgetViewStyle) {
+      self.slide = slide
+      self.scale = scale
+      self.widgetViewStyle = widgetViewStyle
+
+     let slideID: PersistentIdentifier = slide.persistentModelID
+        
+      // Compare relationship directly:
+      _textWidgets = Query(
+        filter:    #Predicate<TextWidget> { textWidget in
+            textWidget.slide?.persistentModelID == slideID
+        },
+        sort:      [SortDescriptor(\.z)],
+        animation: .default
+      )
+      _instrumentWidgets = Query(
+        filter:    #Predicate<InstrumentWidget> { instrumentWidget in
+            instrumentWidget.slide?.persistentModelID == slideID
+        },
+        sort:      [SortDescriptor(\.z)],
+        animation: .default
+      )
+    }
 
     var body: some View {
         ZStack(alignment: .topLeading) {
-            ForEach(slide.widgets, id: \.id) { widget in
+            ForEach(allWidgets, id: \.id) { widget in
                 switch widgetViewStyle {
                 case .thumbnail:
                     WidgetThumbnail(widget: widget, scale: scale)
